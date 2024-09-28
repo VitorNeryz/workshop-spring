b94f5c617b75bc4049de486811759f7a2629bf41
Vitor Nery <vitormanoel2020@gmail.com>
2024-09-28 16:15:07 -0300

    Camada de serviço e registro de componentes

---
+++ src/main/java/com/nery/LearningSpringBoot/services/UserService.java
@@ -0,0 +1,25 @@
+package com.nery.LearningSpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nery.LearningSpringBoot.entities.User;
+import com.nery.LearningSpringBoot.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
