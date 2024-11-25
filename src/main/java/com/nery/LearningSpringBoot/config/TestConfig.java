ddcbc512df0060e7a6db627b3075ea9989a40d61
Vitor Nery <vitormanoel2020@gmail.com>
2024-11-25 14:10:24 -0300

    Payment, Associação OneToOne com Order

--- src/main/java/com/nery/LearningSpringBoot/config/TestConfig.java
+++ src/main/java/com/nery/LearningSpringBoot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nery.LearningSpringBoot.entities.Category;
 import com.nery.LearningSpringBoot.entities.Order;
 import com.nery.LearningSpringBoot.entities.OrderItem;
+import com.nery.LearningSpringBoot.entities.Payment;
 import com.nery.LearningSpringBoot.entities.Product;
 import com.nery.LearningSpringBoot.entities.User;
 import com.nery.LearningSpringBoot.enums.OrderStatus;
@@ -79,5 +80,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T17:53:07Z"),o2);
+		o2.setPayment(pay1);
+		orderRepository.save(o2);
 	}
 }
