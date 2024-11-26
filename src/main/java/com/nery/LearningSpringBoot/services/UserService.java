4a0d8c1a6a1e0aafb169fe8d298657b022132b87
Vitor Nery <vitormanoel2020@gmail.com>
2024-11-25 21:58:58 -0300

    Tratando excessões em operações de Update

--- src/main/java/com/nery/LearningSpringBoot/services/UserService.java
+++ src/main/java/com/nery/LearningSpringBoot/services/UserService.java
@@ -13,6 +13,8 @@ import com.nery.LearningSpringBoot.repositories.UserRepository;
 import com.nery.LearningSpringBoot.services.exceptions.DatabaseException;
 import com.nery.LearningSpringBoot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id,User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {			
+			User entity = repository.getReferenceById(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
